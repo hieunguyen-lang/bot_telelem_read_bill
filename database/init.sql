444562715aa076ea5e89e0a33ca925bd55595798
=hieunguyen-lang <=hieu0356600737@gmail.com>
2025-07-01 11:41:21 +0700

    r

--- database/init.sql
+++ database/init.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS thong_tin_hoa_don (
     ly_do VARCHAR(255),
     dia_chi VARCHAR(300),
     stk_khach VARCHAR(300),
-    khach_moi boolean default FALSE null
+    khach_moi boolean default FALSE null,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
