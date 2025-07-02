b466d16044262e2a41ae7b7156bda86e3f227c91
hieunguyen-lang <hieu0356600737@gmail.com>
2025-07-02 07:27:35 +0700

    t

--- handlers/photo_handler.py
+++ handlers/photo_handler.py
@@ -1,5 +1,5 @@
 from telegram.ext import MessageHandler, Filters,CommandHandler
-from core import handle_photo,start_image_mode
+from core import handle_photo
 
 def register_photo_handler(dp):
     dp.add_handler(MessageHandler(Filters.photo, handle_photo))
