e1a3e49b8c5eb86dfab8ed1a41500bfa457c4fa5
=hieunguyen-lang <=hieu0356600737@gmail.com>
2025-07-29 10:47:43 +0700

    u

--- utils/report_sender.py
+++ utils/report_sender.py
@@ -3,6 +3,7 @@ from datetime import datetime,timedelta
 import os
 from data_connect.mysql_db_connector import MySQLConnector
 import pytz
+from helpers import helper 
 db = MySQLConnector(
     host=os.getenv("MYSQL_HOST"),
     user=os.getenv("MYSQL_USER"),
@@ -18,9 +19,19 @@ def send_daily_report(bot: Bot, chat_id: int):
     tomorrow_day = str(tomorrow.day)
     print(f"Đang gửi báo cáo hàng ngày cho ngày {tomorrow_day}...")
     query = """
-        SELECT nguoi_gui, ten_khach, ngay_giao_dich, so_dien_thoai
-        FROM thong_tin_hoa_don
-        WHERE lich_canh_bao = %s
+        SELECT
+            MAX(nguoi_gui) AS nguoi_gui,
+            MAX(ten_khach) AS ten_khach,
+            MAX(thoi_gian) AS thoi_gian,
+            so_dien_thoai,
+            sum(tong_so_tien) AS tong_tien
+        FROM
+            thong_tin_hoa_don
+        WHERE
+            lich_canh_bao = %s
+        GROUP BY
+            so_dien_thoai;
+
     """
     results = db.fetchall(query, (tomorrow_day,))
 
@@ -37,7 +48,8 @@ def send_daily_report(bot: Bot, chat_id: int):
         lines.append(
             f"*#{i} - {row['ten_khach']}*\n"
             f"👤 Gửi bởi: @{row['nguoi_gui']}\n"
-            f"🗓 Ngày GD: {row['ngay_giao_dich']} | ☎ {row['so_dien_thoai']}\n"
+            f"🗓 Ngày GD: {row['thoi_gian']} - ☎ {row['so_dien_thoai']}\n"
+            f"🗓 Tổng tiền đáo/rút: {helper.format_currency_vn(row['tong_tien'])} vnđ\n"
             "-------------------"
         )
 
@@ -45,7 +57,7 @@ def send_daily_report(bot: Bot, chat_id: int):
     report = "\n".join(lines)
 
     bot.send_message(
-        chat_id=chat_id,
+        chat_id=-4748164025,
         text=f"📆 *Lịch hẹn ngày mai ({tomorrow_str})*: Dưới đây là danh sách khách có giao dịch đáo/rút bạn cần lưu ý:\n\n" + report,
         parse_mode="Markdown"
     )
